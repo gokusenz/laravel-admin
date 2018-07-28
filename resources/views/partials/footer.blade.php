780a7a1d8d6a9ceb7fa71e35e6c3b45d9490343c
Nattawut Ruangvivattanaroj <gokusenz@users.noreply.github.com>
2018-07-28 16:56:16 +0700

    Update footer.blade.php

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,9 +1,9 @@
 <!-- Main Footer -->
 <footer class="main-footer">
     <!-- To the right -->
-    <div class="pull-right hidden-xs">
+    <!-- <div class="pull-right hidden-xs">
         <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
-    </div>
+    </div> -->
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    <strong>Version</strong>&nbsp;&nbsp; {!! config('admin.version') !!}
 </footer>
